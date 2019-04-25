78c845dc5d3bb267da5ecf5864ec131222f3e254
Kyle Uehlein <kyleuehlein@gmail.com>
2019-04-25 17:23:02 -0400

    deploy build

---
+++ build/precache-manifest.70de39e25b811f0bf5020812fe1aba1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4136c80ca47eba2d4d9269109aa091d5",
+    "url": "/kuehlein/dow-jones/index.html"
+  },
+  {
+    "revision": "03d7800d723c2e663ed2",
+    "url": "/kuehlein/dow-jones/static/css/main.9b222726.chunk.css"
+  },
+  {
+    "revision": "107597d7cb70196abf6a",
+    "url": "/kuehlein/dow-jones/static/js/2.ab441d11.chunk.js"
+  },
+  {
+    "revision": "03d7800d723c2e663ed2",
+    "url": "/kuehlein/dow-jones/static/js/main.7efcc8fb.chunk.js"
+  },
+  {
+    "revision": "09ee2adf6c702812723a",
+    "url": "/kuehlein/dow-jones/static/js/runtime~main.52eb2aac.js"
+  }
+]);
